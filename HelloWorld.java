b68f97aa6964d4ea4c04205408588ddf8f2d366f
dawsonintella <dawson@intella.co>
2017-03-03 01:01:37 +0800

    16
    16

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,22 +1,5 @@
-import static org.junit.Assert.assertEquals;
-
-import org.testng.annotations.Test;
-
-
 public class HelloWorld {
-	
-   String a = "a:Hello World";	
-   String b = "b:Hello World";
-   String c = "c:hello world";
-
-   @Test
-   public void test_ab() {
-      assertEquals(a,b);
-   }
-   
-   @Test
-   public void test_ac() {
-	      assertEquals(a,c);
-   }
-   
+    public static void main(String[] args) {
+        System.out.println("Hello! World!");
+    }
 }
